9f0cee89900b6355389148a24dee7327807568fc
Angel Alonso <andelous.aa@gmail.com>
2025-05-22 00:00:48 -0600

    Servlet skeleton

---
+++ src/main/java/com/angeld/kafkaapp/servlet/MainServlet.java
@@ -0,0 +1,8 @@
+package com.angeld.kafkaapp.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 4427019123484161199L;
+
+}

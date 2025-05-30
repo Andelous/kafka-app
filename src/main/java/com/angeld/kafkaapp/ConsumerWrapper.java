9ca3e915d8c5ecf33b610b51d64638b341e78eb4
Angel Alonso <andelous.aa@gmail.com>
2025-05-29 21:47:35 -0600

    Integrating MongoDB events publishing.

--- src/main/java/com/angeld/kafkaapp/ConsumerWrapper.java
+++ src/main/java/com/angeld/kafkaapp/ConsumerWrapper.java
@@ -4,15 +4,30 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.bson.Document;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;;
 
 public class ConsumerWrapper {
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+	private static final TypeReference<HashMap<String, String>> REF = new TypeReference<HashMap<String, String>>() {
+	};
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerWrapper.class);
 
 	private KafkaConsumer<String, String> consumer;
@@ -21,26 +36,51 @@ public class ConsumerWrapper {
 	private boolean shouldRun = true;
 	private Long lastConsumed;
 
+	private MongoClient mongoClient;
+	private MongoDatabase database;
+	private MongoCollection<Document> collection;
+
 	public ConsumerWrapper(String name) {
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(KafkaObjects.CONSUMER_PROPERTIES);
 		consumer.subscribe(Arrays.asList(KafkaObjects.CONSUMER_PROPERTIES.getProperty("topic")));
 
+		mongoClient = MongoClients.create(KafkaObjects.CONSUMER_PROPERTIES.getProperty("mongo.uri"));
+		database = mongoClient.getDatabase(KafkaObjects.CONSUMER_PROPERTIES.getProperty("mongo.database"));
+		collection = database.getCollection(KafkaObjects.CONSUMER_PROPERTIES.getProperty("mongo.collection"));
+
 		Runnable r = new Runnable() {
 			@Override
 			public void run() {
-				while (shouldRun) {
-					ConsumerRecords<String, String> val = consumer.poll(Duration.ofMillis(100));
+				try {
+					while (shouldRun) {
+						ConsumerRecords<String, String> val = consumer.poll(Duration.ofMillis(100));
 
-					for (ConsumerRecord<String, String> record : val) {
-						LOGGER.info("offset = {}, key = {}, value = {}", record.offset(), record.key(), record.value());
-						events.add(record.value());
-						lastConsumed = Instant.now().toEpochMilli();
+						for (ConsumerRecord<String, String> record : val) {
+							LOGGER.info("Processing new EVENT key = {}, value = {}", record.key(), record.value());
+							events.add(record.value());
+
+							Map<String, String> event = MAPPER.readValue(record.value(), REF);
+
+							Document doc1 = new Document(event).append(KafkaObjects.CONSUMER,
+									ConsumerWrapper.this.name);
+							InsertOneResult result = collection.insertOne(doc1);
+
+							LOGGER.info("Inserted a document with the following id: "
+									+ result.getInsertedId().asObjectId().getValue());
+
+							lastConsumed = Instant.now().toEpochMilli();
+						}
 					}
+				} catch (Exception e) {
+					LOGGER.error("Something went wrong...", e);
+					throw new RuntimeException(e);
 				}
 			}
 		};
 
-		new Thread(r).start();
+		Thread t = new Thread(r);
+		t.setName(KafkaObjects.CONSUMER + "-" + name);
+		t.start();
 
 		this.consumer = consumer;
 		this.name = name;
